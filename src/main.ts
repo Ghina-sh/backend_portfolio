4fe010f2af9acf0561f7478f46d0ffb21bd741dc
ghina10 <sharafghena@gmail.com>
2024-10-09 01:57:43 +0300

    first commit

--- src/main.ts
+++ src/main.ts
@@ -1,41 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express';
-
-const server = express();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-  app.enableCors(); // Enable CORS for cross-origin requests
-  app.setGlobalPrefix('api'); // Set the global prefix for routes
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.init();
+  const port = process.env.DB_PORT;
+  await app.listen(port);
 }
 bootstrap();
-
-// Export server for Vercel's serverless function
-export default server;
-
-/*
-import { NestFactory } from '@nestjs/core';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import { AppModule } from './app.module';
-import * as express from 'express';
-import { Server } from 'http';
-
-const server = express();
-
-export const createNestServer = async (expressInstance) => {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(expressInstance));
-  app.enableCors(); // Enable CORS if needed
-  return app.init();
-};
-
-createNestServer(server);
-
-const app = new Server(server);
-export default app;
-
-*/
